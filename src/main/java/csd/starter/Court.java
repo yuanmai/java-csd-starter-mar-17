34e37141fa347906b9c848dd9a24656312453c7c
zengk <392997195@qq.com>
2017-03-05 11:58:53 +0800

    场地类

---
+++ src/main/java/csd/starter/Court.java
@@ -0,0 +1,11 @@
+package csd.starter;
+
+/**
+ * Created by zengk on 2017/3/5.
+ */
+public class Court {
+    public String courtName;
+    public int price;
+    public int[] courtTimes;
+    public  int[] courtXY;
+}
---
+++ src/main/java/csd/starter/curtService.java
@@ -0,0 +1,15 @@
+package csd.starter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zengk on 2017/3/5.
+ */
+public class CurtService {
+    public List getCurtTimeAndFee(){
+        return null;
+    }
+
+}
