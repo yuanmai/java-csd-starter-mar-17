dcf1c0e659342905b398019af3e5f2987edbb81c
KevinYangCHN <yckchn@gmail.com>
2017-03-05 13:36:34 +0800

    1. order

--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -1,8 +1,9 @@
 package csd.starter;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
 
@@ -10,21 +11,41 @@ public class Main {
         boolean stop = true;
 
         while (stop) {
-            System.out.print("choose your role (P / M):");
+            System.out.print("choose your role (P / M): ");
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                 String role = RoleSelector.select(in.readLine());
                 if (role.equals("M")) {
-                    // TODO 绠＄悊鍛�
+                    // TODO 管理员
                 } else if (role.equals("P")) {
-                    // TODO 鐢ㄦ埛
+                    // TODO 用户
+                    handlePlayer(in);
                 } else {
                     System.out.println("illegal role");
                 }
 
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                // NOOP
             }
+        }
+    }
+
+    private static void handlePlayer(BufferedReader in) throws Exception {
+        System.out.print("Enter your username: ");
+        String user = in.readLine();
+        System.out.print("Enter order date: ");
+        String startTimeStr = in.readLine();
+        System.out.print("Enter order duration: ");
+        Integer order = Integer.valueOf(in.readLine());
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date startTime = format.parse(startTimeStr);
+
+        OrderService orderService = new OrderService();
+        if (orderService.order(user, startTime, order)) {
+            System.out.println("Order successfully.");
+        } else {
+            System.out.println("Order fail.");
         }
     }
 
