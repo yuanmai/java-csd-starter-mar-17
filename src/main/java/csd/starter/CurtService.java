a5154d162886375dbb7cba47fe5d75869e0c6fe8
zengk <392997195@qq.com>
2017-03-05 16:49:45 +0800

    场地服务类

--- src/main/java/csd/starter/CurtService.java
+++ src/main/java/csd/starter/CurtService.java
@@ -1,15 +1,31 @@
 package csd.starter;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-/**
- * Created by zengk on 2017/3/5.
- */
+
+
 public class CurtService {
-    public List getCurtTimeAndFee(){
-        return null;
-    }
+    public List<Court> getCurtTimeAndFee(){
+        List<Court> courtList = new ArrayList<Court>();
 
+        Court courtA = new Court();
+        courtA.courtName = "courtA";
+        courtA.price = 10;
+        courtA.courtXY =  new int[]{1,2};
+
+        Court courtB = new Court();
+        courtA.courtName = "courtB";
+        courtA.price = 20;
+        courtA.courtXY =  new int[]{10,20};
+
+        Court courtC = new Court();
+        courtA.courtName = "courtC";
+        courtA.price = 5;
+        courtA.courtXY =  new int[]{50,20};
+        courtList.add(courtA);
+        courtList.add(courtB);
+        courtList.add(courtC);
+        return  courtList;
+    }
 }
