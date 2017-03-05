2a8c56a43b83f6bd2030c807c8e7a725b431c27b
Lucian Lin <luxian.lin@ygccw.com>
2017-03-05 16:29:39 +0800

    add p2p test for main

--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -1,11 +1,22 @@
 package csd.starter;
 
+
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		//		System.setIn();
-		//		System.setOut();
-	}
+//        Stack
+        System.setIn(System.in);
+        System.setOut(System.out);
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str);
+
+
+
+    }
 
 }
