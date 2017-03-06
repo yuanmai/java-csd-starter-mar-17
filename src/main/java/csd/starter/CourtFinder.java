73dce1cde6fc998d732933a401f08d0e18010f3f
pandalxb <fzuxx@qq.com>
2017-03-06 15:21:46 +0800

    查找最近场地API优化

--- src/main/java/csd/starter/CourtFinder.java
+++ src/main/java/csd/starter/CourtFinder.java
@@ -11,9 +11,8 @@ public class CourtFinder {
      * @param latitude
      * @return
      */
-    public static int findCourt(double longitude, double latitude) {
+    public static int findNearestCourt(double longitude, double latitude) {
         int id = 0;
-        //return (int) (longitude / 5);
         double minDistance = 999999999;
         for (int i = 0; i < CourtDatabase.courtList.size(); i++) {
             Court court = CourtDatabase.courtList.get(i);
