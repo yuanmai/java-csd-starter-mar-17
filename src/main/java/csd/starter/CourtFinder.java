37447729f37c8aaf1b00341a01338401d2f436ec
KevinYangCHN <yckchn@gmail.com>
2017-03-06 10:57:25 +0800

    修改单元测试类名

--- src/main/java/csd/starter/CourtFinder.java
+++ src/main/java/csd/starter/CourtFinder.java
@@ -1,14 +1,12 @@
 package csd.starter;
 
-import java.util.List;
-import java.util.ArrayList;
-
 /**
  * Created by CK on 2017/3/5.
  */
 public class CourtFinder {
     /**
      * pandalxb
+     *
      * @param longitude
      * @param latitude
      * @return
@@ -16,12 +14,12 @@ public class CourtFinder {
     public static int findCourt(double longitude, double latitude) {
         int id = 0;
         //return (int) (longitude / 5);
-        double minDinstance = 999999999;
+        double minDistance = 999999999;
         for (int i = 0; i < CourtDatabase.courtList.size(); i++) {
             Court court = CourtDatabase.courtList.get(i);
-            double dinstance = (longitude - court.getLongitude())*(longitude - court.getLongitude()) + (latitude - court.getLatitude())*(latitude - court.getLatitude());
-            if(dinstance < minDinstance){
-                minDinstance = dinstance;
+            double distance = (longitude - court.getLongitude()) * (longitude - court.getLongitude()) + (latitude - court.getLatitude()) * (latitude - court.getLatitude());
+            if (distance < minDistance) {
+                minDistance = distance;
                 id = court.getId();
             }
         }
