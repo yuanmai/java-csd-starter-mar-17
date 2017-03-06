7d90985d92175eaff5d7253bfaef47296187218e
suzf <suzf@chinanetcenter.com>
2017-03-06 11:15:22 +0800

    main commit

--- src/main/java/csd/starter/Main.java
+++ src/main/java/csd/starter/Main.java
@@ -7,16 +7,37 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//        Stack
-        System.setIn(System.in);
-        System.setOut(System.out);
+		index();
+	}
 
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        System.out.println(str);
+	public static void index() {
 
+		System.out.println("请输入功能编号：");
+		System.out.println("1.预约场地");
+		System.out.println("2.查看场地信息");
+		System.out.println("0.退出");
 
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
 
-    }
+		if (str.equals("1")) {
+
+			System.out.println("我是预约");
+		}
+		else if (str.equals("2")) {
+
+			System.out.println("我是场地");
+		}
+		else if (str.equals("0")) {
+
+			System.out.println("谢谢使用");
+		}
+		else {
+
+			System.out.println("请输入正确的编号！");
+			index();
+		}
+
+	}
 
 }
