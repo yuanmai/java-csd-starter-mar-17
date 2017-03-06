81dcf2b7352b190bfc9451859f26749ecde9f2c4
linyihang <linyihang123@gmail.com>
2017-03-06 09:09:31 +0800

    第一个单元测试用例

--- src/test/java/com/reserve/ReserveStepDefines.java
+++ src/test/java/com/reserve/ReserveStepDefines.java
@@ -1,18 +1,30 @@
 package com.reserve;
 
+import csd.starter.Main;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 
+import java.io.*;
+
 /**
  * Created by linyihang on 2017/3/5.
  */
 public class ReserveStepDefines {
+
     @Given("^i input \"([^\"]*)\" and \"([^\"]*)\" and \"([^\"]*)\" and \"([^\"]*)\"$")
     public void i_input_and(String arg1, String arg2, String arg3, String arg4) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
+
+        String in = arg1 + " " + arg2 + " " + arg3 + " " + arg4;
+        System.setIn(new BufferedInputStream(new ByteArrayInputStream(in.getBytes("UTF-8"))));
+        System.setOut(new PrintStream(baos));
+        Main.main(null);
+        System.setIn(oldIn);
+        System.setOut(oldOut);
+        content = baos.toString();
     }
 
     @When("^no others subcibed$")
@@ -25,5 +37,12 @@ public class ReserveStepDefines {
     public void the_out_put_should_be(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         //throw new PendingException();
+        Assert.assertEquals(arg1, content);
+        System.out.println(content);
     }
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    InputStream oldIn = System.in;
+    PrintStream oldOut = System.out;
+    String content;
 }
