7907cea9e141ce63b296e199364d4f13cb2779fa
Lucian Lin <luxian.lin@ygccw.com>
2017-03-06 09:49:36 +0800

    improve MainTest.

--- src/test/java/csd/starter/MainTest.java
+++ src/test/java/csd/starter/MainTest.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
@@ -25,18 +25,26 @@ public class MainTest {
 
     @Test
     public void uat() {
-        Scanner scanner = new Scanner("fake input");
-        Scanner scanner1 = new Scanner(System.in);
 
         String inputStr = "123344";
+
+        InputStream stdin = System.in;
+        PrintStream stdout = System.out;
+        OutputStream outputStream = new ByteArrayOutputStream();
         InputStream inputStream = new ByteArrayInputStream(inputStr.getBytes());
-        System.setIn(inputStream);
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(byteArrayOutputStream));
-        Main.main(null);
+        try {
+            System.setIn(inputStream);
+            System.setOut(new PrintStream(outputStream));
+            Main.main(null);
 
-        assertEquals(inputStr,byteArrayOutputStream.toString().trim());
+            assertEquals(inputStr, outputStream.toString().trim());
+        }
+        finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
     }
+
 
 }
