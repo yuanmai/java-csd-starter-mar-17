ad26d107d26d2b738c0cfb89e65c87ef65f29ed7
zengk <392997195@qq.com>
2017-03-06 11:52:39 +0800

    场地服务单元测试

--- src/test/java/csd/starter/CourtServiceTest.java
+++ src/test/java/csd/starter/CourtServiceTest.java
@@ -1,5 +1,6 @@
 package csd.starter;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,6 +12,8 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class CourtServiceTest {
+    CurtService courtService = new CurtService();
+
     @Before
     public void Before(){
         System.out.println("Before test Court");
@@ -18,10 +21,34 @@ public class CourtServiceTest {
 
     @Test
     public void test_Court_fee_greater_than_zero(){
-        CurtService courtService = new CurtService();
         List<Court> courts = courtService.getCurtTimeAndFee();
+
         assertTrue(courts.get(0).price>0);
     }
 
+    @Test(expected = Exception.class)
+    public void test_pub_price_exception() throws Exception {
+        courtService.pub("须毛球馆",-1,new int[]{0,0});
+    }
 
+
+    @Test
+    public void test_pub_success() throws Exception {
+        String msg = courtService.pub("须毛球馆",10,new int[]{0,0});
+
+        assertEquals("pub succss", msg);
+    }
+
+    @Test
+    public void test_pub_fail() throws Exception {
+        courtService.pub("须毛球馆",10,new int[]{0,0});
+        String msg = courtService.pub("须毛球馆",10,new int[]{0,0});
+
+        assertEquals("pub fail", msg);
+    }
+
+    @After
+    public void After(){
+        courtService.resetPubList();
+    }
 }
